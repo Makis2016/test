a59855377cca0009e134180c664b362d123d9cbb
makis2016 <806534160@qq.com>
2020-10-24 20:30:39 +0800

    hello word

--- source/demo/src/main/java/com/example/demo/controllers/TestController.java
+++ source/demo/src/main/java/com/example/demo/controllers/TestController.java
@@ -11,6 +11,6 @@ public class TestController {
     @GetMapping("/m1")
     public String test(){
 
-        return "hello word 1111 lalal1212190900";
+        return "hello word jenkins";
     }
 }
